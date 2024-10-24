e8e68e28e7b12ddf45dd624345d81d3658a5da43
Diegoo1802 <ifpdiegovigara@gmail.com>
2024-10-24 09:34:39 +0200

    Proyecto1

---
+++ Proyecto1_Cs/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Proyecto1_Cs
+{
+    partial class FormularioPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_form2 = new System.Windows.Forms.Button();
+            this.btn_form3 = new System.Windows.Forms.Button();
+            this.btn_form4 = new System.Windows.Forms.Button();
+            this.btn_form5 = new System.Windows.Forms.Button();
+            this.btn_cv = new System.Windows.Forms.Button();
+            this.btn_web = new System.Windows.Forms.Button();
+            this.btn_enlace = new System.Windows.Forms.Button();
+            this.btn_imagen = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_form2
+            // 
+            this.btn_form2.Location = new System.Drawing.Point(58, 60);
+            this.btn_form2.Name = "btn_form2";
+            this.btn_form2.Size = new System.Drawing.Size(95, 38);
+            this.btn_form2.TabIndex = 0;
+            this.btn_form2.Text = "Ir a Formulario 2";
+            this.btn_form2.UseVisualStyleBackColor = true;
+            this.btn_form2.Click += new System.EventHandler(this.btn_form2_Click);
+            // 
+            // btn_form3
+            // 
+            this.btn_form3.Location = new System.Drawing.Point(176, 60);
+            this.btn_form3.Name = "btn_form3";
+            this.btn_form3.Size = new System.Drawing.Size(91, 38);
+            this.btn_form3.TabIndex = 1;
+            this.btn_form3.Text = "Ir a Formulario 3";
+            this.btn_form3.UseVisualStyleBackColor = true;
+            this.btn_form3.Click += new System.EventHandler(this.btn_form3_Click);
+            // 
+            // btn_form4
+            // 
+            this.btn_form4.Location = new System.Drawing.Point(286, 60);
+            this.btn_form4.Name = "btn_form4";
+            this.btn_form4.Size = new System.Drawing.Size(95, 38);
+            this.btn_form4.TabIndex = 2;
+            this.btn_form4.Text = "Ir a Formulario 4";
+            this.btn_form4.UseVisualStyleBackColor = true;
+            this.btn_form4.Click += new System.EventHandler(this.btn_form4_Click);
+            // 
+            // btn_form5
+            // 
+            this.btn_form5.Location = new System.Drawing.Point(387, 60);
+            this.btn_form5.Name = "btn_form5";
+            this.btn_form5.Size = new System.Drawing.Size(101, 38);
+            this.btn_form5.TabIndex = 3;
+            this.btn_form5.Text = "Ir a Formulario 5";
+            this.btn_form5.UseVisualStyleBackColor = true;
+            this.btn_form5.Click += new System.EventHandler(this.btn_form5_Click);
+            // 
+            // btn_cv
+            // 
+            this.btn_cv.Location = new System.Drawing.Point(494, 60);
+            this.btn_cv.Name = "btn_cv";
+            this.btn_cv.Size = new System.Drawing.Size(94, 38);
+            this.btn_cv.TabIndex = 4;
+            this.btn_cv.Text = "Ir al CV";
+            this.btn_cv.UseVisualStyleBackColor = true;
+            this.btn_cv.Click += new System.EventHandler(this.btn_cv_Click);
+            // 
+            // btn_web
+            // 
+            this.btn_web.Location = new System.Drawing.Point(595, 60);
+            this.btn_web.Name = "btn_web";
+            this.btn_web.Size = new System.Drawing.Size(86, 38);
+            this.btn_web.TabIndex = 5;
+            this.btn_web.Text = "Ir a la Web";
+            this.btn_web.UseVisualStyleBackColor = true;
+            this.btn_web.Click += new System.EventHandler(this.btn_web_Click);
+            // 
+            // btn_enlace
+            // 
+            this.btn_enlace.Location = new System.Drawing.Point(58, 168);
+            this.btn_enlace.Name = "btn_enlace";
+            this.btn_enlace.Size = new System.Drawing.Size(95, 38);
+            this.btn_enlace.TabIndex = 6;
+            this.btn_enlace.Text = "Ir al enlace interno";
+            this.btn_enlace.UseVisualStyleBackColor = true;
+            this.btn_enlace.Click += new System.EventHandler(this.btn_enlace_Click);
+            // 
+            // btn_imagen
+            // 
+            this.btn_imagen.Location = new System.Drawing.Point(176, 168);
+            this.btn_imagen.Name = "btn_imagen";
+            this.btn_imagen.Size = new System.Drawing.Size(91, 38);
+            this.btn_imagen.TabIndex = 7;
+            this.btn_imagen.Text = "Ir a una imagen";
+            this.btn_imagen.UseVisualStyleBackColor = true;
+            this.btn_imagen.Click += new System.EventHandler(this.btn_imagen_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(286, 168);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(95, 38);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(387, 168);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(101, 38);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(494, 168);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(94, 38);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(595, 168);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(86, 38);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(267, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // FormularioPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.btn_imagen);
+            this.Controls.Add(this.btn_enlace);
+            this.Controls.Add(this.btn_web);
+            this.Controls.Add(this.btn_cv);
+            this.Controls.Add(this.btn_form5);
+            this.Controls.Add(this.btn_form4);
+            this.Controls.Add(this.btn_form3);
+            this.Controls.Add(this.btn_form2);
+            this.Name = "FormularioPrincipal";
+            this.Text = "FormularioPrincipal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_form2;
+        private System.Windows.Forms.Button btn_form3;
+        private System.Windows.Forms.Button btn_form4;
+        private System.Windows.Forms.Button btn_form5;
+        private System.Windows.Forms.Button btn_cv;
+        private System.Windows.Forms.Button btn_web;
+        private System.Windows.Forms.Button btn_enlace;
+        private System.Windows.Forms.Button btn_imagen;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Proyecto1_Cs/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto1_Cs
+{
+    public partial class FormularioPrincipal : Form
+    {
+        public FormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_form2_Click(object sender, EventArgs e)
+        {
+            //Creamos un nuevo formulario que al llamarlo se abra pulsado el boton
+            Formulario2 nuevoForm2 = new Formulario2();
+            nuevoForm2.Show(); 
+        }
+
+        private void btn_form3_Click(object sender, EventArgs e)
+        {
+            Formulario3 nuevoForm3 = new Formulario3();
+            nuevoForm3.Show();  
+        }
+
+        private void btn_form4_Click(object sender, EventArgs e)
+        {
+            Formulario4 nuevoForm4 = new Formulario4(); 
+            nuevoForm4.Show();
+        }
+
+        private void btn_form5_Click(object sender, EventArgs e)
+        {
+            Formulario5 nuevoForm5 = new Formulario5();
+            nuevoForm5.Show();
+        }
+
+        private void btn_cv_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_web_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_enlace_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_imagen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
