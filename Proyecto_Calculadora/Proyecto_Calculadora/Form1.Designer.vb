6c9db7dd93ed438a6832a98a8fcb8e637e5f829c
Diegoo1802 <ifpdiegovigara@gmail.com>
2024-10-24 13:55:01 +0200

    Calculadora en proceso

---
+++ Proyecto_Calculadora/Proyecto_Calculadora/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_c = New System.Windows.Forms.Button()
+        Me.btn_0 = New System.Windows.Forms.Button()
+        Me.btn_1 = New System.Windows.Forms.Button()
+        Me.btn_4 = New System.Windows.Forms.Button()
+        Me.btn_7 = New System.Windows.Forms.Button()
+        Me.btn_8 = New System.Windows.Forms.Button()
+        Me.btn_5 = New System.Windows.Forms.Button()
+        Me.btn_2 = New System.Windows.Forms.Button()
+        Me.btn_coma = New System.Windows.Forms.Button()
+        Me.btn_po = New System.Windows.Forms.Button()
+        Me.btn_div = New System.Windows.Forms.Button()
+        Me.btn_rest = New System.Windows.Forms.Button()
+        Me.btn_sum = New System.Windows.Forms.Button()
+        Me.btn_mult = New System.Windows.Forms.Button()
+        Me.btn_9 = New System.Windows.Forms.Button()
+        Me.btn_6 = New System.Windows.Forms.Button()
+        Me.btn_3 = New System.Windows.Forms.Button()
+        Me.btn_igual = New System.Windows.Forms.Button()
+        Me.btn_pot = New System.Windows.Forms.Button()
+        Me.txt_Display = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn_c
+        '
+        Me.btn_c.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_c.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_c.Location = New System.Drawing.Point(29, 121)
+        Me.btn_c.Name = "btn_c"
+        Me.btn_c.Size = New System.Drawing.Size(58, 58)
+        Me.btn_c.TabIndex = 12
+        Me.btn_c.Text = "C"
+        Me.btn_c.UseVisualStyleBackColor = False
+        '
+        'btn_0
+        '
+        Me.btn_0.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_0.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_0.Location = New System.Drawing.Point(29, 377)
+        Me.btn_0.Name = "btn_0"
+        Me.btn_0.Size = New System.Drawing.Size(58, 58)
+        Me.btn_0.TabIndex = 13
+        Me.btn_0.Text = "0"
+        Me.btn_0.UseVisualStyleBackColor = False
+        '
+        'btn_1
+        '
+        Me.btn_1.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_1.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_1.Location = New System.Drawing.Point(29, 313)
+        Me.btn_1.Name = "btn_1"
+        Me.btn_1.Size = New System.Drawing.Size(58, 58)
+        Me.btn_1.TabIndex = 14
+        Me.btn_1.Text = "1"
+        Me.btn_1.UseVisualStyleBackColor = False
+        '
+        'btn_4
+        '
+        Me.btn_4.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_4.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_4.Location = New System.Drawing.Point(29, 249)
+        Me.btn_4.Name = "btn_4"
+        Me.btn_4.Size = New System.Drawing.Size(58, 58)
+        Me.btn_4.TabIndex = 15
+        Me.btn_4.Text = "4"
+        Me.btn_4.UseVisualStyleBackColor = False
+        '
+        'btn_7
+        '
+        Me.btn_7.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_7.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_7.Location = New System.Drawing.Point(29, 185)
+        Me.btn_7.Name = "btn_7"
+        Me.btn_7.Size = New System.Drawing.Size(58, 58)
+        Me.btn_7.TabIndex = 16
+        Me.btn_7.Text = "7"
+        Me.btn_7.UseVisualStyleBackColor = False
+        '
+        'btn_8
+        '
+        Me.btn_8.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_8.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_8.Location = New System.Drawing.Point(102, 185)
+        Me.btn_8.Name = "btn_8"
+        Me.btn_8.Size = New System.Drawing.Size(58, 58)
+        Me.btn_8.TabIndex = 21
+        Me.btn_8.Text = "8"
+        Me.btn_8.UseVisualStyleBackColor = False
+        '
+        'btn_5
+        '
+        Me.btn_5.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_5.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_5.Location = New System.Drawing.Point(102, 249)
+        Me.btn_5.Name = "btn_5"
+        Me.btn_5.Size = New System.Drawing.Size(58, 58)
+        Me.btn_5.TabIndex = 20
+        Me.btn_5.Text = "5"
+        Me.btn_5.UseVisualStyleBackColor = False
+        '
+        'btn_2
+        '
+        Me.btn_2.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_2.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_2.Location = New System.Drawing.Point(102, 313)
+        Me.btn_2.Name = "btn_2"
+        Me.btn_2.Size = New System.Drawing.Size(58, 58)
+        Me.btn_2.TabIndex = 19
+        Me.btn_2.Text = "2"
+        Me.btn_2.UseVisualStyleBackColor = False
+        '
+        'btn_coma
+        '
+        Me.btn_coma.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_coma.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_coma.Location = New System.Drawing.Point(102, 377)
+        Me.btn_coma.Name = "btn_coma"
+        Me.btn_coma.Size = New System.Drawing.Size(58, 58)
+        Me.btn_coma.TabIndex = 18
+        Me.btn_coma.Text = ","
+        Me.btn_coma.UseVisualStyleBackColor = False
+        '
+        'btn_po
+        '
+        Me.btn_po.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_po.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_po.Location = New System.Drawing.Point(102, 121)
+        Me.btn_po.Name = "btn_po"
+        Me.btn_po.Size = New System.Drawing.Size(58, 58)
+        Me.btn_po.TabIndex = 17
+        Me.btn_po.Text = "%"
+        Me.btn_po.UseVisualStyleBackColor = False
+        '
+        'btn_div
+        '
+        Me.btn_div.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_div.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_div.Location = New System.Drawing.Point(249, 185)
+        Me.btn_div.Name = "btn_div"
+        Me.btn_div.Size = New System.Drawing.Size(58, 58)
+        Me.btn_div.TabIndex = 31
+        Me.btn_div.Text = "/"
+        Me.btn_div.UseVisualStyleBackColor = False
+        '
+        'btn_rest
+        '
+        Me.btn_rest.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_rest.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_rest.Location = New System.Drawing.Point(249, 249)
+        Me.btn_rest.Name = "btn_rest"
+        Me.btn_rest.Size = New System.Drawing.Size(58, 58)
+        Me.btn_rest.TabIndex = 30
+        Me.btn_rest.Text = "-"
+        Me.btn_rest.UseVisualStyleBackColor = False
+        '
+        'btn_sum
+        '
+        Me.btn_sum.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_sum.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_sum.Location = New System.Drawing.Point(249, 313)
+        Me.btn_sum.Name = "btn_sum"
+        Me.btn_sum.Size = New System.Drawing.Size(58, 58)
+        Me.btn_sum.TabIndex = 29
+        Me.btn_sum.Text = "+"
+        Me.btn_sum.UseVisualStyleBackColor = False
+        '
+        'btn_mult
+        '
+        Me.btn_mult.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_mult.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_mult.Location = New System.Drawing.Point(249, 121)
+        Me.btn_mult.Name = "btn_mult"
+        Me.btn_mult.Size = New System.Drawing.Size(58, 58)
+        Me.btn_mult.TabIndex = 27
+        Me.btn_mult.Text = "X"
+        Me.btn_mult.UseVisualStyleBackColor = False
+        '
+        'btn_9
+        '
+        Me.btn_9.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_9.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_9.Location = New System.Drawing.Point(176, 185)
+        Me.btn_9.Name = "btn_9"
+        Me.btn_9.Size = New System.Drawing.Size(58, 58)
+        Me.btn_9.TabIndex = 26
+        Me.btn_9.Text = "9"
+        Me.btn_9.UseVisualStyleBackColor = False
+        '
+        'btn_6
+        '
+        Me.btn_6.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_6.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_6.Location = New System.Drawing.Point(176, 249)
+        Me.btn_6.Name = "btn_6"
+        Me.btn_6.Size = New System.Drawing.Size(58, 58)
+        Me.btn_6.TabIndex = 25
+        Me.btn_6.Text = "6"
+        Me.btn_6.UseVisualStyleBackColor = False
+        '
+        'btn_3
+        '
+        Me.btn_3.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.btn_3.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_3.Location = New System.Drawing.Point(176, 313)
+        Me.btn_3.Name = "btn_3"
+        Me.btn_3.Size = New System.Drawing.Size(58, 58)
+        Me.btn_3.TabIndex = 24
+        Me.btn_3.Text = "3"
+        Me.btn_3.UseVisualStyleBackColor = False
+        '
+        'btn_igual
+        '
+        Me.btn_igual.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_igual.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_igual.Location = New System.Drawing.Point(176, 377)
+        Me.btn_igual.Name = "btn_igual"
+        Me.btn_igual.Size = New System.Drawing.Size(131, 58)
+        Me.btn_igual.TabIndex = 23
+        Me.btn_igual.Text = "="
+        Me.btn_igual.UseVisualStyleBackColor = False
+        '
+        'btn_pot
+        '
+        Me.btn_pot.BackColor = System.Drawing.Color.Moccasin
+        Me.btn_pot.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btn_pot.Location = New System.Drawing.Point(176, 121)
+        Me.btn_pot.Name = "btn_pot"
+        Me.btn_pot.Size = New System.Drawing.Size(58, 58)
+        Me.btn_pot.TabIndex = 22
+        Me.btn_pot.Text = "^"
+        Me.btn_pot.UseVisualStyleBackColor = False
+        '
+        'txt_Display
+        '
+        Me.txt_Display.Enabled = False
+        Me.txt_Display.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Display.Location = New System.Drawing.Point(29, 53)
+        Me.txt_Display.Name = "txt_Display"
+        Me.txt_Display.Size = New System.Drawing.Size(278, 62)
+        Me.txt_Display.TabIndex = 32
+        Me.txt_Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(324, 450)
+        Me.Controls.Add(Me.txt_Display)
+        Me.Controls.Add(Me.btn_div)
+        Me.Controls.Add(Me.btn_rest)
+        Me.Controls.Add(Me.btn_sum)
+        Me.Controls.Add(Me.btn_mult)
+        Me.Controls.Add(Me.btn_9)
+        Me.Controls.Add(Me.btn_6)
+        Me.Controls.Add(Me.btn_3)
+        Me.Controls.Add(Me.btn_igual)
+        Me.Controls.Add(Me.btn_pot)
+        Me.Controls.Add(Me.btn_8)
+        Me.Controls.Add(Me.btn_5)
+        Me.Controls.Add(Me.btn_2)
+        Me.Controls.Add(Me.btn_coma)
+        Me.Controls.Add(Me.btn_po)
+        Me.Controls.Add(Me.btn_7)
+        Me.Controls.Add(Me.btn_4)
+        Me.Controls.Add(Me.btn_1)
+        Me.Controls.Add(Me.btn_0)
+        Me.Controls.Add(Me.btn_c)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_c As Button
+    Friend WithEvents btn_0 As Button
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_coma As Button
+    Friend WithEvents btn_po As Button
+    Friend WithEvents btn_div As Button
+    Friend WithEvents btn_rest As Button
+    Friend WithEvents btn_sum As Button
+    Friend WithEvents btn_mult As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_igual As Button
+    Friend WithEvents btn_pot As Button
+    Friend WithEvents txt_Display As TextBox
+End Class
---
+++ Proyecto_Calculadora/Proyecto_Calculadora/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn_0_Click(sender As Button, e As EventArgs) Handles btn_coma.Click, btn_9.Click, btn_8.Click, btn_7.Click, btn_6.Click, btn_5.Click, btn_4.Click, btn_3.Click, btn_2.Click, btn_1.Click, btn_0.Click
+        txt_Display.Text += sender.Text
+    End Sub
+
+    Private Sub btn_c_Click(sender As Object, e As EventArgs) Handles btn_c.Click
+
+    End Sub
+
+    Private Sub btn_po_Click(sender As Object, e As EventArgs) Handles btn_po.Click
+
+    End Sub
+
+    Private Sub btn_pot_Click(sender As Object, e As EventArgs) Handles btn_pot.Click
+
+    End Sub
+
+    Private Sub btn_mult_Click(sender As Object, e As EventArgs) Handles btn_mult.Click
+
+    End Sub
+
+    Private Sub btn_div_Click(sender As Object, e As EventArgs) Handles btn_div.Click
+
+    End Sub
+
+    Private Sub btn_rest_Click(sender As Object, e As EventArgs) Handles btn_rest.Click
+
+    End Sub
+
+    Private Sub btn_sum_Click(sender As Object, e As EventArgs) Handles btn_sum.Click
+
+    End Sub
+End Class
