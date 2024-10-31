78d7d6da1567e03c619421aa633253ab4de8bfda
Diegoo1802 <ifpdiegovigara@gmail.com>
2024-10-31 13:48:29 +0100

    Calculadora acabada

--- Proyecto_Calculadora/Proyecto_Calculadora/Form1.Designer.vb
+++ Proyecto_Calculadora/Proyecto_Calculadora/Form1.Designer.vb
@@ -50,7 +50,7 @@ Partial Class Form1
         Me.btn_c.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_c.Location = New System.Drawing.Point(29, 121)
         Me.btn_c.Name = "btn_c"
-        Me.btn_c.Size = New System.Drawing.Size(58, 58)
+        Me.btn_c.Size = New System.Drawing.Size(61, 62)
         Me.btn_c.TabIndex = 12
         Me.btn_c.Text = "C"
         Me.btn_c.UseVisualStyleBackColor = False
@@ -61,7 +61,7 @@ Partial Class Form1
         Me.btn_0.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_0.Location = New System.Drawing.Point(29, 377)
         Me.btn_0.Name = "btn_0"
-        Me.btn_0.Size = New System.Drawing.Size(58, 58)
+        Me.btn_0.Size = New System.Drawing.Size(61, 62)
         Me.btn_0.TabIndex = 13
         Me.btn_0.Text = "0"
         Me.btn_0.UseVisualStyleBackColor = False
@@ -72,7 +72,7 @@ Partial Class Form1
         Me.btn_1.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_1.Location = New System.Drawing.Point(29, 313)
         Me.btn_1.Name = "btn_1"
-        Me.btn_1.Size = New System.Drawing.Size(58, 58)
+        Me.btn_1.Size = New System.Drawing.Size(61, 62)
         Me.btn_1.TabIndex = 14
         Me.btn_1.Text = "1"
         Me.btn_1.UseVisualStyleBackColor = False
@@ -83,7 +83,7 @@ Partial Class Form1
         Me.btn_4.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_4.Location = New System.Drawing.Point(29, 249)
         Me.btn_4.Name = "btn_4"
-        Me.btn_4.Size = New System.Drawing.Size(58, 58)
+        Me.btn_4.Size = New System.Drawing.Size(61, 62)
         Me.btn_4.TabIndex = 15
         Me.btn_4.Text = "4"
         Me.btn_4.UseVisualStyleBackColor = False
@@ -94,7 +94,7 @@ Partial Class Form1
         Me.btn_7.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_7.Location = New System.Drawing.Point(29, 185)
         Me.btn_7.Name = "btn_7"
-        Me.btn_7.Size = New System.Drawing.Size(58, 58)
+        Me.btn_7.Size = New System.Drawing.Size(61, 62)
         Me.btn_7.TabIndex = 16
         Me.btn_7.Text = "7"
         Me.btn_7.UseVisualStyleBackColor = False
@@ -105,7 +105,7 @@ Partial Class Form1
         Me.btn_8.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_8.Location = New System.Drawing.Point(102, 185)
         Me.btn_8.Name = "btn_8"
-        Me.btn_8.Size = New System.Drawing.Size(58, 58)
+        Me.btn_8.Size = New System.Drawing.Size(61, 62)
         Me.btn_8.TabIndex = 21
         Me.btn_8.Text = "8"
         Me.btn_8.UseVisualStyleBackColor = False
@@ -116,7 +116,7 @@ Partial Class Form1
         Me.btn_5.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_5.Location = New System.Drawing.Point(102, 249)
         Me.btn_5.Name = "btn_5"
-        Me.btn_5.Size = New System.Drawing.Size(58, 58)
+        Me.btn_5.Size = New System.Drawing.Size(61, 62)
         Me.btn_5.TabIndex = 20
         Me.btn_5.Text = "5"
         Me.btn_5.UseVisualStyleBackColor = False
@@ -127,7 +127,7 @@ Partial Class Form1
         Me.btn_2.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_2.Location = New System.Drawing.Point(102, 313)
         Me.btn_2.Name = "btn_2"
-        Me.btn_2.Size = New System.Drawing.Size(58, 58)
+        Me.btn_2.Size = New System.Drawing.Size(61, 62)
         Me.btn_2.TabIndex = 19
         Me.btn_2.Text = "2"
         Me.btn_2.UseVisualStyleBackColor = False
@@ -138,7 +138,7 @@ Partial Class Form1
         Me.btn_coma.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_coma.Location = New System.Drawing.Point(102, 377)
         Me.btn_coma.Name = "btn_coma"
-        Me.btn_coma.Size = New System.Drawing.Size(58, 58)
+        Me.btn_coma.Size = New System.Drawing.Size(61, 62)
         Me.btn_coma.TabIndex = 18
         Me.btn_coma.Text = ","
         Me.btn_coma.UseVisualStyleBackColor = False
@@ -149,7 +149,7 @@ Partial Class Form1
         Me.btn_po.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_po.Location = New System.Drawing.Point(102, 121)
         Me.btn_po.Name = "btn_po"
-        Me.btn_po.Size = New System.Drawing.Size(58, 58)
+        Me.btn_po.Size = New System.Drawing.Size(61, 62)
         Me.btn_po.TabIndex = 17
         Me.btn_po.Text = "%"
         Me.btn_po.UseVisualStyleBackColor = False
@@ -160,7 +160,7 @@ Partial Class Form1
         Me.btn_div.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_div.Location = New System.Drawing.Point(249, 185)
         Me.btn_div.Name = "btn_div"
-        Me.btn_div.Size = New System.Drawing.Size(58, 58)
+        Me.btn_div.Size = New System.Drawing.Size(61, 62)
         Me.btn_div.TabIndex = 31
         Me.btn_div.Text = "/"
         Me.btn_div.UseVisualStyleBackColor = False
@@ -171,7 +171,7 @@ Partial Class Form1
         Me.btn_rest.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_rest.Location = New System.Drawing.Point(249, 249)
         Me.btn_rest.Name = "btn_rest"
-        Me.btn_rest.Size = New System.Drawing.Size(58, 58)
+        Me.btn_rest.Size = New System.Drawing.Size(61, 62)
         Me.btn_rest.TabIndex = 30
         Me.btn_rest.Text = "-"
         Me.btn_rest.UseVisualStyleBackColor = False
@@ -182,7 +182,7 @@ Partial Class Form1
         Me.btn_sum.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_sum.Location = New System.Drawing.Point(249, 313)
         Me.btn_sum.Name = "btn_sum"
-        Me.btn_sum.Size = New System.Drawing.Size(58, 58)
+        Me.btn_sum.Size = New System.Drawing.Size(61, 62)
         Me.btn_sum.TabIndex = 29
         Me.btn_sum.Text = "+"
         Me.btn_sum.UseVisualStyleBackColor = False
@@ -193,7 +193,7 @@ Partial Class Form1
         Me.btn_mult.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_mult.Location = New System.Drawing.Point(249, 121)
         Me.btn_mult.Name = "btn_mult"
-        Me.btn_mult.Size = New System.Drawing.Size(58, 58)
+        Me.btn_mult.Size = New System.Drawing.Size(61, 62)
         Me.btn_mult.TabIndex = 27
         Me.btn_mult.Text = "X"
         Me.btn_mult.UseVisualStyleBackColor = False
@@ -204,7 +204,7 @@ Partial Class Form1
         Me.btn_9.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_9.Location = New System.Drawing.Point(176, 185)
         Me.btn_9.Name = "btn_9"
-        Me.btn_9.Size = New System.Drawing.Size(58, 58)
+        Me.btn_9.Size = New System.Drawing.Size(61, 62)
         Me.btn_9.TabIndex = 26
         Me.btn_9.Text = "9"
         Me.btn_9.UseVisualStyleBackColor = False
@@ -215,7 +215,7 @@ Partial Class Form1
         Me.btn_6.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_6.Location = New System.Drawing.Point(176, 249)
         Me.btn_6.Name = "btn_6"
-        Me.btn_6.Size = New System.Drawing.Size(58, 58)
+        Me.btn_6.Size = New System.Drawing.Size(61, 62)
         Me.btn_6.TabIndex = 25
         Me.btn_6.Text = "6"
         Me.btn_6.UseVisualStyleBackColor = False
@@ -226,7 +226,7 @@ Partial Class Form1
         Me.btn_3.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_3.Location = New System.Drawing.Point(176, 313)
         Me.btn_3.Name = "btn_3"
-        Me.btn_3.Size = New System.Drawing.Size(58, 58)
+        Me.btn_3.Size = New System.Drawing.Size(61, 62)
         Me.btn_3.TabIndex = 24
         Me.btn_3.Text = "3"
         Me.btn_3.UseVisualStyleBackColor = False
@@ -237,7 +237,7 @@ Partial Class Form1
         Me.btn_igual.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_igual.Location = New System.Drawing.Point(176, 377)
         Me.btn_igual.Name = "btn_igual"
-        Me.btn_igual.Size = New System.Drawing.Size(131, 58)
+        Me.btn_igual.Size = New System.Drawing.Size(134, 62)
         Me.btn_igual.TabIndex = 23
         Me.btn_igual.Text = "="
         Me.btn_igual.UseVisualStyleBackColor = False
@@ -248,7 +248,7 @@ Partial Class Form1
         Me.btn_pot.Font = New System.Drawing.Font("Goudy Stout", 20.25!, System.Drawing.FontStyle.Bold)
         Me.btn_pot.Location = New System.Drawing.Point(176, 121)
         Me.btn_pot.Name = "btn_pot"
-        Me.btn_pot.Size = New System.Drawing.Size(58, 58)
+        Me.btn_pot.Size = New System.Drawing.Size(61, 62)
         Me.btn_pot.TabIndex = 22
         Me.btn_pot.Text = "^"
         Me.btn_pot.UseVisualStyleBackColor = False
@@ -259,7 +259,7 @@ Partial Class Form1
         Me.txt_Display.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txt_Display.Location = New System.Drawing.Point(29, 53)
         Me.txt_Display.Name = "txt_Display"
-        Me.txt_Display.Size = New System.Drawing.Size(278, 62)
+        Me.txt_Display.Size = New System.Drawing.Size(281, 62)
         Me.txt_Display.TabIndex = 32
         Me.txt_Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
@@ -268,7 +268,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveBorder
-        Me.ClientSize = New System.Drawing.Size(324, 450)
+        Me.ClientSize = New System.Drawing.Size(327, 454)
         Me.Controls.Add(Me.txt_Display)
         Me.Controls.Add(Me.btn_div)
         Me.Controls.Add(Me.btn_rest)
--- Proyecto_Calculadora/Proyecto_Calculadora/Form1.vb
+++ Proyecto_Calculadora/Proyecto_Calculadora/Form1.vb
@@ -1,37 +1,146 @@
 ﻿Public Class Form1
+    ' Variables para almacenar el primer número, segundo número y la operación seleccionada
+    Private firstNum As Double  ' Primer número ingresado por el usuario
+    Private secondNum As Double ' Segundo número ingresado por el usuario
+    Private operation As String ' Operación matemática seleccionada (+, -, *, /, etc.)
+    Private isOperationPerformed As Boolean = False ' Indica si una operación ya fue seleccionada
+
+    ' Al cargar el formulario, establecemos el texto inicial de la pantalla en "0"
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        txt_Display.Text = "0"
     End Sub
 
-    Private Sub btn_0_Click(sender As Button, e As EventArgs) Handles btn_coma.Click, btn_9.Click, btn_8.Click, btn_7.Click, btn_6.Click, btn_5.Click, btn_4.Click, btn_3.Click, btn_2.Click, btn_1.Click, btn_0.Click
-        txt_Display.Text += sender.Text
+    ' **Botones de números**
+    ' Cada botón de número llama a la función AppendNumber con el número correspondiente
+    Private Sub btn_0_Click(sender As Object, e As EventArgs) Handles btn_0.Click
+        AppendNumber("0")
     End Sub
 
-    Private Sub btn_c_Click(sender As Object, e As EventArgs) Handles btn_c.Click
-
+    Private Sub btn_1_Click(sender As Object, e As EventArgs) Handles btn_1.Click
+        AppendNumber("1")
     End Sub
 
-    Private Sub btn_po_Click(sender As Object, e As EventArgs) Handles btn_po.Click
-
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        AppendNumber("2")
     End Sub
 
-    Private Sub btn_pot_Click(sender As Object, e As EventArgs) Handles btn_pot.Click
-
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        AppendNumber("3")
     End Sub
 
-    Private Sub btn_mult_Click(sender As Object, e As EventArgs) Handles btn_mult.Click
-
+    Private Sub btn_4_Click(sender As Object, e As EventArgs) Handles btn_4.Click
+        AppendNumber("4")
     End Sub
 
-    Private Sub btn_div_Click(sender As Object, e As EventArgs) Handles btn_div.Click
+    Private Sub btn_5_Click(sender As Object, e As EventArgs) Handles btn_5.Click
+        AppendNumber("5")
+    End Sub
 
+    Private Sub btn_6_Click(sender As Object, e As EventArgs) Handles btn_6.Click
+        AppendNumber("6")
+    End Sub
+
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        AppendNumber("7")
+    End Sub
+
+    Private Sub btn_8_Click(sender As Object, e As EventArgs) Handles btn_8.Click
+        AppendNumber("8")
+    End Sub
+
+    Private Sub btn_9_Click(sender As Object, e As EventArgs) Handles btn_9.Click
+        AppendNumber("9")
+    End Sub
+
+    ' Función para agregar números al display
+    Private Sub AppendNumber(num As String)
+        ' Si el display muestra "0" o si ya se seleccionó una operación, reemplaza el display con el número
+        If txt_Display.Text = "0" OrElse isOperationPerformed Then
+            txt_Display.Text = num
+            isOperationPerformed = False ' Reseteamos el indicador de operación
+        Else
+            txt_Display.Text &= num ' Concatenamos el número al display
+        End If
+    End Sub
+
+    ' **Punto decimal**
+    Private Sub btn_coma_Click(sender As Object, e As EventArgs) Handles btn_coma.Click
+        ' Agrega un punto decimal solo si aún no existe en el display
+        If Not txt_Display.Text.Contains(".") Then
+            txt_Display.Text &= "."
+        End If
+    End Sub
+
+    ' **Operaciones**
+    ' Cada botón de operación llama a PerformOperation con el símbolo correspondiente
+    Private Sub btn_sum_Click(sender As Object, e As EventArgs) Handles btn_sum.Click
+        PerformOperation("+")
     End Sub
 
     Private Sub btn_rest_Click(sender As Object, e As EventArgs) Handles btn_rest.Click
-
+        PerformOperation("-")
     End Sub
 
-    Private Sub btn_sum_Click(sender As Object, e As EventArgs) Handles btn_sum.Click
-
+    Private Sub btn_mult_Click(sender As Object, e As EventArgs) Handles btn_mult.Click
+        PerformOperation("*")
     End Sub
+
+    Private Sub btn_div_Click(sender As Object, e As EventArgs) Handles btn_div.Click
+        PerformOperation("/")
+    End Sub
+
+    Private Sub btn_pot_Click(sender As Object, e As EventArgs) Handles btn_pot.Click
+        PerformOperation("^")
+    End Sub
+
+    Private Sub btn_po_Click(sender As Object, e As EventArgs) Handles btn_po.Click
+        PerformOperation("%")
+    End Sub
+
+    ' Función que se llama cuando se selecciona una operación matemática (+, -, *, /, etc.)
+    Private Sub PerformOperation(op As String)
+        firstNum = Double.Parse(txt_Display.Text) ' Almacena el número actual en firstNum
+        operation = op ' Almacena el tipo de operación seleccionada
+        isOperationPerformed = True ' Indica que ya se seleccionó una operación
+    End Sub
+
+    ' **Botón igual**
+    Private Sub btn_igual_Click(sender As Object, e As EventArgs) Handles btn_igual.Click
+        secondNum = Double.Parse(txt_Display.Text) ' Toma el segundo número del display
+        Dim result As Double ' Variable para almacenar el resultado
+
+        ' Dependiendo de la operación seleccionada, realiza el cálculo correspondiente
+        Select Case operation
+            Case "+" ' Suma
+                result = firstNum + secondNum
+            Case "-" ' Resta
+                result = firstNum - secondNum
+            Case "*" ' Multiplicación
+                result = firstNum * secondNum
+            Case "/" ' División
+                If secondNum <> 0 Then
+                    result = firstNum / secondNum
+                Else
+                    MessageBox.Show("No se puede dividir por cero")
+                    Exit Sub
+                End If
+            Case "^" ' Potencia
+                result = Math.Pow(firstNum, secondNum)
+            Case "%" ' Porcentaje
+                result = firstNum * (secondNum / 100)
+        End Select
+
+        txt_Display.Text = result.ToString() ' Muestra el resultado en el display
+        isOperationPerformed = True ' Marca que se ha realizado una operación
+    End Sub
+
+    ' **Botón de limpiar (C)**
+    Private Sub btn_c_Click(sender As Object, e As EventArgs) Handles btn_c.Click
+        txt_Display.Text = "0" ' Resetea el display a "0"
+        firstNum = 0 ' Resetea el primer número
+        secondNum = 0 ' Resetea el segundo número
+        operation = "" ' Resetea la operación seleccionada
+        isOperationPerformed = False ' Resetea el indicador de operación realizada
+    End Sub
+
 End Class
